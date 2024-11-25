4db489381b1c6f8ad4ad28fa7b6caa79fc6f0664
Ayush Nagar <ayushnagar15tfe@gmail.com>
2024-11-25 15:43:55 +0530

    add: number guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+mod guessing_game;
+
+use guessing_game::guessing_game;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print_menu();
+
+        println!("Enter your choice:");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line!");
+
+        let choice: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!\n");
+                continue;
+            }
+        };
+
+        match choice {
+            0 => break,
+            1 => guessing_game(),
+            _ => println!("Invalid choice!\n"),
+        }
+    }
+}
+
+fn print_menu() {
+    println!("Which program would you like to run? (Enter number)");
+    println!("0. Exit");
+    println!("1. Guessing Game\n");
 }
